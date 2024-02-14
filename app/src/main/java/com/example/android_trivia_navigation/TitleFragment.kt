14e4296fbc4f637a52a4a7099483e8db7694ebb0
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2024-02-14 22:40:15 +0100

    navigate from one fragment to another

--- app/src/main/java/com/example/android_trivia_navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android_trivia_navigation/TitleFragment.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.android_trivia_navigation.databinding.ActivityMainBinding
+import com.example.android_trivia_navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
+    private lateinit var binding: FragmentTitleBinding
 
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_title, container, false)
+            binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+            binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+            )
+
+            return binding.root
+
         }
 }
 
